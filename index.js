5f18bc5a56c637b0ec7fd22de1339105a177fb85
mdharoonhussain <haroonhussain97@gmail.com>
2022-10-21 00:05:19 +0530

    added_2_programs_successfully

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("it is a prime number");
+// Check whether a number is prime or not
+   let num = 5;
+   let count = 0;
+
+    for(let i = 0;i<=num;i++){
+     if(num%i==0){
+      count++;
+     }
+   }
+    if(count==2){
+    console.log(num, "is prime");
+    else{
+     console.log(num, "is not prime");
+
+// Check whether a string is palindrome or not.
+
+   let str = "racecar";
+   var new_str = "";
+   let N = 7;
+   
+   for(let i= N;i>=0;i--){
+    new_str = new_str + str[i];
+   }
+   if(str==new_str){
+    console.log("Yes");
+   } else{
+     console.log("No");
+   }
